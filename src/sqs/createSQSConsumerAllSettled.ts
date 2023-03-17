be4c03406c49d2894937ef5739f66fb5b7f513ec
Diego Carvalho <direisc@gmail.com>
2023-03-17 14:13:25 -0300

    doc: update crate sqs consumer documentation
    add better example and alerts

--- src/sqs/createSQSConsumerAllSettled.ts
+++ src/sqs/createSQSConsumerAllSettled.ts
@@ -2,17 +2,27 @@ import { type SQSHandler, type SQSBatchItemFailure, type SQSRecord } from 'aws-l
 type Runner = (record: SQSRecord) => Promise<void>
 
 /**
- * createSQSConsumerAllSettled
- * Create consumer with all needs to run SQS consumer and a batch failure
+ * # createSQSConsumerAllSettled
+ *
+ * Create consumer with all needs to run SQS consumer and a batch failure.
  *
  * Can catch errors if are expected avoid throw errors,
  * error indicating failure couldn't catching.
  *
+ * ## ⚠ SQS trigger with lambda require set Report Batch Item Failures
+ *
  * @example
  * ```
  * export const main = createSQSConsumerAllSettled(async (record) => {
- *   // TODO Implementation for consume message record
  *   // don't need handle errors with try catch block
+ *   const body = record.body
+ *   if (body === undefined || body.trim().length === 0) {
+ *     // Process with failure, but don't need retry
+ *     console.error('Record body is empty')
+ *     return
+ *   }
+ *   // TODO Implementation for consume message record
+ *   console.log('Process Record:', body)
  * })
  * ```
  */
@@ -24,7 +34,7 @@ export const createSQSConsumerAllSettled =
         try {
           await runner(record)
         } catch (error) {
-          console.error('PROCESS ERROR: ', error)
+          console.error('RUNNER ERROR: ', error)
           throw record
         }
       }),
